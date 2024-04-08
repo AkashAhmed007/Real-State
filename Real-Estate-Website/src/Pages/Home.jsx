e12a96755eeafb1547a1642839733310e40bdc9a
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-04-08 20:26:01 +0600

    banner section added

--- Real-Estate-Website/src/Pages/Home.jsx
+++ Real-Estate-Website/src/Pages/Home.jsx
@@ -1,11 +1,40 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
+
 const Home = () => {
-    return (
-        <div>
-            <div  className="image">
-            <img className="w-1/2 h-1/4 mx-auto" src="../../public/bg-image.jpeg" alt="HomeImage" />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Swiper
+        navigation={true}
+        Pagination={true}
+        modules={[Navigation, Autoplay, Pagination]}
+        loop={true}
+        autoplay={{ delay: 2000 }}
+      >
+        <SwiperSlide>
+          <div className="slide1 slider">
+            <h1 className="text-8xl font-extrabold text-black">Driven By Integrity</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide2 slider">
+            <h1>Slide2</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide3 slider">
+            <h1>Slide3</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide4 slider">
+            <h1>Slide4</h1>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Home;
